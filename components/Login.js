583ff5cae99e9c6538f0d55e714581a330ddf3d9
PrangonChakraborty-1805085 <1805085@ugrad.cse.buet.ac.bd>
2021-07-20 12:24:30 +0600

    Second Commit

--- components/Login.js
+++ components/Login.js
@@ -5,7 +5,7 @@ function Login() {
         <div className="grid place-items-center">
             <Image src="https://links.papareact.com/t4i" width={400} height={400} objectFit="contain"/>
 
-            <h1 onClick={signIn} className="text-center p-5 rounded-full bg-blue-500 cursor-pointer text-white">Login with Facebook</h1>
+            <h1 onClick={signIn} className="text-center p-5 rounded-full bg-blue-500 cursor-pointer text-white hover:bg-blue-700 active:bg-blue-900">Login with Facebook</h1>
         </div>
     )
 }
